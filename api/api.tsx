4f6a788f37fed2ff06e279f1d0e0f2c41118444b
Lucas Souza <lucas_vss@hotmail.com>
2023-01-24 12:58:09 -0300

    feat: api consumer

---
+++ api/api.tsx
@@ -0,0 +1,11 @@
+export const GetProducts = async () => {
+  const url =
+    "https://mks-challenge-api-frontend.herokuapp.com/api/v1/products?page=1&rows=50&sortBy=id&orderBy=ASC";
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data
+  } catch (error) {
+    console.log("Error: " + error);
+  }
+};
