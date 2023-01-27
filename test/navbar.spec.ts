1a7e3364673245d2540625c04c36482c1f8dd3fc
Lucas Souza <lucas_vss@hotmail.com>
2023-01-27 17:40:43 -0300

    feat: install react testing library and jest types

---
+++ test/navbar.spec.ts
@@ -0,0 +1,9 @@
+import { describe } from "node:test";
+import { render } from "@testing-library/react";
+import NavComponent from "../components/NavComponent"
+
+describe("AsideNav", () => {
+  test("render corretly", () => {
+    render(<div>a</div>);
+  });
+});
